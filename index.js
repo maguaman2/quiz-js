0af7dc3625c0dc6e8114b701210d32711f802ce1
Marco Guaman <magbmail@gmail.com>
2023-07-05 11:26:15 -0500

    cadena de caracteres

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+let nums = [2,5,1,5];
+for (let i=0; i<nums.length;i++){
+    console.log(nums[i]);
+}
+
+//La variable de tipo String se convierte un array(cadena) de caracteres
+let myName = "Aurelio";
+for (let i=0; i<myName.length;i++){
+    console.log(myName[i]);
+}
